617cb7fa65c8563f328f8656e512f69820f9e651
skampp <clint.martin.3@gmail.com>
2025-06-25 19:43:54 -0400

    Updated from home

--- Backend/4.5 Capstone/index.js
+++ Backend/4.5 Capstone/index.js
@@ -4,7 +4,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-
+// I'm shaky on the "/view/:filename" bit.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
