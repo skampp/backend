a12df4dfe826620ddb7648c9066f6668f984d981
skampp <clint.martin.3@gmail.com>
2025-07-06 18:45:15 -0400

    Thumper - Users incorporated loosely

--- Backend/_permaTemp/index.js
+++ Backend/_permaTemp/index.js
@@ -12,6 +12,7 @@ const db = new pg.Client({
 
 const app = express();
 const port = 3000;
+var userLogged = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,6 +20,7 @@ app.use(express.static("public"));
 let passage = [];
 var startup = false;
 await db.connect(); // optional if already connected
+var uSearch = "SELECT * FROM thuser";
 
 if ((startup = true)) {
   var sSearch =
@@ -28,26 +30,41 @@ if ((startup = true)) {
     "SELECT book_stats.book, * FROM nkjv JOIN book_stats on nkjv.refbook = book_stats.abbrev where refbook = '2Ti' and refchapter = 3 and refverse > 15 and refverse < 18";
 }
 
-async function thumperSearch(sSearch) {
+async function thumperSearch(sSearch, myField, myID) {
   try {
-    const result = await db.query(sSearch); // no callback here
-    // console.log(result.rows[0]); // you now have access to the rows
-    return result.rows; // just return the data you want
+    const result = await db.query(sSearch);
+    return result.rows;
   } catch (err) {
     console.error("Error executing query", err.stack);
-    return []; // or handle error as appropriate
+    return [];
   }
 }
 
 app.get("/", async (req, res) => {
-  const resultVerses = await thumperSearch(sSearch);
-  // console.log(resultVerses);
-  res.render("index.ejs", { myPassage: resultVerses });
+  // Combined from INDEX and USERS, so far so good
+  try {
+    const resultVerses = await thumperSearch(sSearch);
+    const resultUsers = await thumperSearch(uSearch);
+    // console.log(resultVerses);
+    res.render("index.ejs", {
+      myPassage: resultVerses,
+      userLogged: userLogged,
+      myUsers: resultUsers,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/submit", async (req, res) => {
-  // The next two sections really need to be in one or even two functions, but scoping is kicking my butt.  Help.
+  // From INDEX only, incorporate USERS variables
   var fullString = req.body.answer;
+  if (userLogged) {
+    var resultUsers = JSON.parse(req.body.resultUsers);
+    var resultVerses = JSON.parse(req.body.resultVerses);
+  }
+
+  // var userLogged = JSON.parse(req.body.userLogged);
   // To handle something with both 1 and 2 spaces, such as 'Genesis 1:1' and '1 Corinthians 1:1'.
   // Future issue to handle: multiples such as '1 Corinthians 1:1-5, 7-10'
   const lastSpace = fullString.lastIndexOf(" ");
@@ -55,24 +72,109 @@ app.post("/submit", async (req, res) => {
   var fullString = fullString.slice(lastSpace + 1);
   var [sChapter, sVerse] = fullString.split(":");
   var [sStartVerse, sEndVerse] = sVerse.split("-");
-  
+
   var sSearch =
     "SELECT book_stats.book, * FROM nkjv JOIN book_stats on nkjv.refbook = book_stats.abbrev ";
   if (sBook.length == 3) {
-    sSearch = sSearch + "where lower(refbook) = '" + sBook.toLowerCase() + "'";
+    sSearch += "where lower(refbook) = '" + sBook.toLowerCase() + "'";
   } else {
-    sSearch = sSearch + "where lower(book) = '" + sBook.toLowerCase() + "'";
+    sSearch += "where lower(book) = '" + sBook.toLowerCase() + "'";
   }
-  sSearch = sSearch + " and refchapter = " + sChapter + " and refverse";
+  sSearch += " and refchapter = " + sChapter + " and refverse";
 
   if (sEndVerse) {
-    sSearch = sSearch + " between " + sStartVerse + " and " + sEndVerse;
+    sSearch += " between " + sStartVerse + " and " + sEndVerse;
   } else {
-    sSearch = sSearch + " = " + sStartVerse;
+    sSearch += " = " + sStartVerse;
   }
-  // console.log("sSearch: " + sSearch);
   const result = await thumperSearch(sSearch);
-  res.render("index.ejs", { myPassage: result });
+  if (userLogged) {
+    res.render("index.ejs", {
+      userLogged: userLogged,
+      myUsers: resultUsers,
+      myVerses: resultVerses,
+      myPassage: result,
+    });
+  } else {
+    res.render("index.ejs", { myPassage: result });
+  }
+});
+
+app.post("/usersubmit", async (req, res) => {
+  // *** CRITICAL ***
+  // What is being passed to index.ejs on any submit?  Those things need to be included here
+  // in order for the verse text to be populated.
+
+  var myResults = JSON.parse(req.body.resultPassage); // necessary when bringing arrays back thru html forms via hidden elements
+  try {
+    if (req.body.listChanged) {
+      // Are we changing the current list?
+      var cSearch =
+        "UPDATE thuser SET defaultlist = '" +
+        req.body.listChoice +
+        "' WHERE userid = " +
+        req.body.userID;
+      await thumperSearch(cSearch);
+    }
+    if (req.body.verses) {
+      // Are we deleting verses to delete?
+      if (Array.isArray(req.body.verses)) {
+        req.body.verses.forEach((verse, i) => {
+          var dSearch = "DELETE FROM thlistpopulation p WHERE id = " + verse;
+          thumperSearch(dSearch);
+        });
+      } else {
+        var dSearch =
+          "DELETE FROM thlistpopulation p WHERE id = " + req.body.verses;
+        await thumperSearch(dSearch);
+      }
+    }
+    if (req.body.addverse) {
+      // Are we adding a verse to the current list?
+      var aSearch =
+        "INSERT INTO thlistpopulation (verselistid, listreference) VALUES ('" +
+        req.body.defaultlist +
+        "', '" +
+        req.body.addverse +
+        "')"; //thlistpopulation.verselistid
+      console.log(aSearch);
+      thumperSearch(aSearch);
+    }
+
+    // User stuff
+
+    var uSearch =
+      "SELECT * FROM thlistsubscription s JOIN thuser u ON s.popuserid = u.userid JOIN thlists l ON l.listid = s.poplistid WHERE u.username = '" +
+      req.body.user +
+      "'";
+    const resultUsers = await thumperSearch(uSearch);
+    userLogged = true;
+
+    // Verse stuff
+    var vSearch =
+      "SELECT * from thlistsubscription s JOIN thlistpopulation p on s.poplistid = p.verselistid WHERE s.poplistid = " +
+      resultUsers[0].defaultlist;
+    const resultVerses = await thumperSearch(vSearch);
+    res.render("index.ejs", {
+      userLogged: userLogged,
+      myUsers: resultUsers,
+      myVerses: resultVerses,
+      myPassage: myResults,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// User logs out
+app.post("/logout", async (req, res) => {
+  try {
+    var myResults = JSON.parse(req.body.resultPassage); // necessary when bringing arrays back thru html forms via hidden elements
+    userLogged = false;
+    res.render("index.ejs", { myPassage: myResults, userLogged: userLogged });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Future use
--- Backend/_permaTemp/users.js
+++ Backend/_permaTemp/users.js
@@ -23,62 +23,82 @@ var uSearch = "SELECT * FROM thuser";
 
 // Startup - User logged out
 app.get("/", async (req, res) => {
-  const resultUsers = await thumperSearch(uSearch);
-  res.render("users.ejs", { userLogged: userLogged, myUsers: resultUsers });
+  try {
+    const resultUsers = await thumperSearch(uSearch);
+    res.render("users.ejs", { userLogged: userLogged, myUsers: resultUsers });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // User logs in (or changes the active list)
 app.post("/usersubmit", async (req, res) => {
-  if (req.body.listChanged) {   // Are we changing the current list?
-    var cSearch =
-      "UPDATE thuser SET defaultlist = '" +
-      req.body.listChoice +
-      "' WHERE userid = " +
-      req.body.userID;
-    await thumperSearch(cSearch);
-  }
-  if (req.body.verses) {    // Are we deleting verses to delete?
-    if (Array.isArray(req.body.verses)) {
-        req.body.verses.forEach((verse, i) => {
-            var dSearch = "DELETE FROM thlistpopulation p WHERE id = " + verse;
-            thumperSearch(dSearch);
-        });
-    } else {
-      var dSearch =
-        "DELETE FROM thlistpopulation p WHERE id = " + req.body.verses;
-      await thumperSearch(dSearch);
+  try {
+    if (req.body.listChanged) {
+      // Are we changing the current list?
+      var cSearch =
+        "UPDATE thuser SET defaultlist = '" +
+        req.body.listChoice +
+        "' WHERE userid = " +
+        req.body.userID;
+      await thumperSearch(cSearch);
     }
-  }
-  if (req.body.addverse) {      // Are we adding a verse to the current list?
-    var aSearch = "INSERT INTO thlistpopulation (verselistid, listreference) VALUES ('" + req.body.defaultlist + "', '" + req.body.addverse + "')"; //thlistpopulation.verselistid
-    console.log(aSearch);
-    thumperSearch(aSearch);
-  }
+    if (req.body.verses) {
+      // Are we deleting verses to delete?
+      if (Array.isArray(req.body.verses)) {
+        req.body.verses.forEach((verse, i) => {
+          var dSearch = "DELETE FROM thlistpopulation p WHERE id = " + verse;
+          thumperSearch(dSearch);
+        });
+      } else {
+        var dSearch =
+          "DELETE FROM thlistpopulation p WHERE id = " + req.body.verses;
+        await thumperSearch(dSearch);
+      }
+    }
+    if (req.body.addverse) {
+      // Are we adding a verse to the current list?
+      var aSearch =
+        "INSERT INTO thlistpopulation (verselistid, listreference) VALUES ('" +
+        req.body.defaultlist +
+        "', '" +
+        req.body.addverse +
+        "')"; //thlistpopulation.verselistid
+      console.log(aSearch);
+      thumperSearch(aSearch);
+    }
 
-  // User stuff
-  var uSearch =
-    "SELECT * FROM thlistsubscription s JOIN thuser u ON s.popuserid = u.userid JOIN thlists l ON l.listid = s.poplistid WHERE u.username = '" +
-    req.body.user +
-    "'";
-  const resultUsers = await thumperSearch(uSearch);
-  userLogged = true;
+    // User stuff
+    var uSearch =
+      "SELECT * FROM thlistsubscription s JOIN thuser u ON s.popuserid = u.userid JOIN thlists l ON l.listid = s.poplistid WHERE u.username = '" +
+      req.body.user +
+      "'";
+    const resultUsers = await thumperSearch(uSearch);
+    userLogged = true;
 
-  // Verse stuff
-  var vSearch =
-    "SELECT * from thlistsubscription s JOIN thlistpopulation p on s.poplistid = p.verselistid WHERE s.poplistid = " +
-    resultUsers[0].defaultlist;
-  const resultVerses = await thumperSearch(vSearch);
-  res.render("users.ejs", {
-    userLogged: userLogged,
-    myUsers: resultUsers,
-    myVerses: resultVerses,
-  });
+    // Verse stuff
+    var vSearch =
+      "SELECT * from thlistsubscription s JOIN thlistpopulation p on s.poplistid = p.verselistid WHERE s.poplistid = " +
+      resultUsers[0].defaultlist;
+    const resultVerses = await thumperSearch(vSearch);
+    res.render("users.ejs", {
+      userLogged: userLogged,
+      myUsers: resultUsers,
+      myVerses: resultVerses,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // User logs out
 app.post("/logout", async (req, res) => {
-  userLogged = false;
-  res.render("users.ejs", { userLogged: userLogged });
+  try {
+    userLogged = false;
+    res.render("users.ejs", { userLogged: userLogged });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Perform any search, and return the results
