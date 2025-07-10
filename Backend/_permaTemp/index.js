3d8eb0ad4703852965cf35984fe51a9bdfc72a23
skampp <clint.martin.3@gmail.com>
2025-07-09 20:25:02 -0400

    Fingers crossed, starting security changes

--- Backend/_permaTemp/index.js
+++ Backend/_permaTemp/index.js
@@ -1,24 +1,42 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "thumper",
-  password: "Cardigan.reboot7",
-  port: 5432,
-});
+import bcrypt from "bcrypt";
+import session from "express-session"
+import passport from "passport";
+import { Strategy} from "passport-local";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
-var userLogged = false;
-var badUser = false;
-var paragraphMode = true;
+const saltRounds = 10;
+env.config();
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+var userLogged = false;
+var badUser = false;
+var paragraphMode = true;
 
 let passage = [];
 var startup = false;
@@ -207,6 +225,83 @@ async function fullName(sBook) {
   const retValue = sBook;
   return retValue;
 }
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  // Need to make a registration form to return the above items.
+  // Manually call it for now.
+
+  try {
+    const checkResult = await db.query("SELECT * FROM thusers WHERE username = $1", [
+      email,
+    ]);
+
+    if (checkResult.rows.length > 0) {
+      req.redirect("/login");
+    } else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error("Error hashing password:", err);
+        } else {
+          const result = await db.query(
+            "INSERT INTO users (username, hashtag, defaultlist) VALUES ($1, $2, 1) RETURNING *",
+            [email, hash]
+          );
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log("success");
+            // Need a landing page, which woudld have been //usersubmit.  Figure out how to work this back into the normal-but-modified flow.
+            res.redirect("/secrets");
+          });
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+passport.use(
+  new Strategy(async function verify(username, password, cb) {
+    try {
+      const result = await db.query("SELECT * FROM thusers WHERE email = $1 ", [
+        username,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            //Error with password check
+            console.error("Error comparing passwords:", err);
+            return cb(err);
+          } else {
+            if (valid) {
+              //Passed password check
+              return cb(null, user);
+            } else {
+              //Did not pass password check
+              return cb(null, false);
+            }
+          }
+        });
+      } else {
+        return cb("User not found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+);
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
